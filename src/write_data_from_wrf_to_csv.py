65bea86451642395fc32ec497e2299548920c76e
Emma Corbett <40502169+ecorbett123@users.noreply.github.com>
2024-09-15 20:58:18 -0400

    edited unused files

--- src/write_data_from_wrf_to_csv.py
+++ src/write_data_from_wrf_to_csv.py
@@ -31,33 +31,6 @@ def visualize_wrf_output(file_path, variable_name):
         np.savetxt('lon_array_2.csv', lons_np, delimiter=',')
         np.savetxt('t2_array_2.csv', t2_np, delimiter=',')
 
-        # Get the cartopy projection
-        # cart_proj = get_cartopy(var)
-        #
-        # # Create the plot
-        # fig = plt.figure(figsize=(12, 9))
-        # ax = plt.axes(projection=cart_proj)
-        # # Remove the existing coastline feature
-        # # Add a detailed coastline feature with a solid line
-        # ax.add_feature(cfeature.COASTLINE, linewidth=0.8, linestyle='-')
-        # ax.add_feature(cfeature.BORDERS, linewidth=0.5)
-        # ax.add_feature(cfeature.STATES, linestyle=':', linewidth=0.5)
-        #
-        # # Plot the variable
-        # contour = plt.contourf(to_np(lons), to_np(lats), to_np(var), cmap="jet", transform=crs.PlateCarree())
-        #
-        # # Add colorbar
-        # plt.colorbar(contour, ax=ax, orientation='horizontal', pad=0.05, aspect=50)
-        #
-        # # Set plot extent
-        # ax.set_extent([to_np(lons).min(), to_np(lons).max(), to_np(lats).min(), to_np(lats).max()])
-        #
-        # # Update plot title with the current time
-        # plt.title(f"{variable_name} at {time}")
-        #
-        # # Display the plot
-        # plt.show()
-
 
 # Paths to the wrfout files for domains d02 and d03
 file_paths = [
